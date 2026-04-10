383f34262b523dfbf02db964a23e46887a5e3574
eusamir <samirandradee@gmail.com>
2026-04-10 11:49:27 -0300

    first commit

---
+++ src/main/java/com/undb/TripManagerUNDB/TripManagerUndbApplication.java
@@ -0,0 +1,13 @@
+package com.undb.TripManagerUNDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripManagerUndbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripManagerUndbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undb/TripManagerUNDB/TripManagerUndbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undb.TripManagerUNDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripManagerUndbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
