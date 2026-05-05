1058e46facd5b93100db220499bda3f76a35d172
Lucas Ribeiro <luxrey26@gmail.com>
2026-05-05 16:01:15 -0300

    chore: remove arquivos vazios

--- src/main/java/com/undb/TripManagerUNDB/auth/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.undb.TripManagerUNDB.auth.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthRequest(
-        @Email @NotBlank String email,
-        @NotBlank String password
-) {}
