77ed9731c7729b1ada6995cdbe4b34c5172eb88e
eusamir <samirandradee@gmail.com>
2026-05-20 02:19:59 -0300

    fix: cors config

--- src/main/java/com/undb/TripManagerUNDB/config/SecurityConfig.java
+++ src/main/java/com/undb/TripManagerUNDB/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(allowedOrigins);
+        cfg.setAllowedOriginPatterns(allowedOrigins);
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
